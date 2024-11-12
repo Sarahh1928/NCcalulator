4eaf9b7fa4cae54f9a61a1fcfba6ceb17c5f5f77
Sarahh1928 <119070393+Sarahh1928@users.noreply.github.com>
2024-11-12 13:53:53 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './index.jsx';
+
+function App() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
